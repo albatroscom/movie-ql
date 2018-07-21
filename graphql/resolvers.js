b4a2c25ed48841133fd412f5421c0aea3d33dd23
albatroscom <albatroscom@naver.com>
2018-07-21 18:30:30 +0900

    add, delete  for mutation

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,6 +1,13 @@
+import { getMovies, getById, addMovie, deleteMovie } from './db';
+
 const resolvers = {
     Query: {
-        name: () => "guspop"
+        movies: () => getMovies(),
+        movie: (_, { id }) => getById(id)
+    },
+    Mutation: {
+        addMovie: (_, {name, score}) => addMovie(name, score),
+        deleteMovie: (_, {id}) => deleteMovie(id)
     }
 };
 
