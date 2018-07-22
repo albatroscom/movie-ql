ac524c82651bf3632bca4582eee505b3c8a0f71c
albatroscom <albatroscom@naver.com>
2018-07-22 19:23:07 +0900

    wrapping a rest api with graphql with axios

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,8 +1,10 @@
-import { getMovies } from './db';
+import { getMovies, getMovie, getSuggestions } from './db';
 
 const resolvers = {
     Query: {
-        movies: (_, {limit, rating}) => getMovies(limit, rating) // _ 이것은 variable 로 여기서는 사용하지 않기 때문에 의미없는 문자로 대체했음
+        movies: (_, {limit, rating}) => getMovies(limit, rating), // _ 이것은 variable 로 여기서는 사용하지 않기 때문에 의미없는 문자로 대체했음
+        movie: (_, {id}) => getMovie(id),
+        suggestions: (_, {id}) => getSuggestions(id)
     }
 };
 
